9f98af0a7d2748f31d5e72c742330003e5026a5d
aarongoen <aarongoen2.0@gmail.com>
2021-04-06 14:29:00 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,9 +4,10 @@ export default class ColorBox extends Component {
 
   render() {
     let opacity = this.props.opacity;
-    return (opacity >= 0.2) ? (<div className="color-box" style={{opacity}}>
-          <ColorBox opacity={opacity - 0.1} />
-        </div>) : null
+    return (
+        <div className="color-box" style={{opacity}}>
+          {opacity >= 0.2 ? <ColorBox opacity={opacity - 0.1} />: null}
+        </div>) 
   }
 }
 
